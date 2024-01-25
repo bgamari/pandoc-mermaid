6cefbe69759446f5d8ba2c5e3d35e495c8b2bbb6
Ben Gamari <ben@smart-cactus.org>
2024-01-25 13:32:30 -0500

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Text as T
+import System.Process
+import Text.Pandoc.JSON
+import Control.Monad.Trans.State
+import Control.Monad.IO.Class
+
+main :: IO ()
+main = evalStateT (toJSONFilter mermaidFilter) (0 :: Int)
+
+mermaidFilter :: Block -> StateT Int IO Block
+mermaidFilter (CodeBlock (ident, classes, kvs) content)
+  | ident == "mermaid" = do
+      n <- get
+      put (succ n)
+      let out = "figure-"<>show n<>".svg"
+      _ <- liftIO $ readProcess "mermaid-cli" ["-i", "-", "-o", out] (T.unpack content)
+      return $ Plain [Image nullAttr [] (T.pack out, "")]
+mermaidFilter blk = return blk
+
